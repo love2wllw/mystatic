a134e122c66165b5e59cb4178d0e0ca9a801a643
wl8lw <love2wllw@qq.com>
2022-01-30 22:18:59 +0800

    Create weila.js

---
+++ tampermonkey_scripts/weila.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         New Userscript
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://www.tampermonkey.net/index.php?locale=zh_CN
+// @icon         https://www.google.com/s2/favicons?domain=tampermonkey.net
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+})();

57c0e70659534863c4468bf9328094093cedc60d
wl8lw <love2wllw@qq.com>
2023-02-17 00:21:35 +0800

    Update and rename weila.js to 1001.js

--- tampermonkey_scripts/1001.js
+++ tampermonkey_scripts/1001.js
@@ -1,10 +1,10 @@
 // ==UserScript==
-// @name         New Userscript
+// @name         pc_ks_ctl
 // @namespace    http://tampermonkey.net/
 // @version      0.1
-// @description  try to take over the world!
-// @author       You
-// @match        https://www.tampermonkey.net/index.php?locale=zh_CN
+// @description  快手PC网页版非首页竖屏全屏播放,鼠标控制翻页
+// @author       DidiLee
+// @match        https://www.kuaishou.com/short-video/*
 // @icon         https://www.google.com/s2/favicons?domain=tampermonkey.net
 // @grant        none
 // ==/UserScript==

def2054c9088c5976be5eb9ffb9a11f0dc68d9a8
love2wllw <love2wllw@qq.com>
2024-01-10 00:34:47 +0800

    add hit-x-hot.js

---
+++ tampermonkey_scripts/hit-x-hot.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         hitxhot
+// @namespace    http://tampermonkey.net/
+// @homepage     https://raw.githubusercontent.com/love2wllw/mystatic/master/tampermonkey_scripts/hit-x-hot.js
+// @version      0.1
+// @description  hitxhot tool
+// @author       DidiLee
+// @match        https://www.hitxhot.org/gallerys/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=hitxhot.org
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+
+})();

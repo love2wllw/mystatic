765359365fb6277091c2fba164476401015f7dc9
love2wllw@qq.com <love2wllw@qq.com>
2024-01-10 01:42:28 +0800

    111

--- tampermonkey_scripts/hit-x-hot.js
+++ tampermonkey_scripts/hit-x-hot.js
@@ -13,16 +13,38 @@
 (function () {
     'use strict';
 
-    async function get_src_list(url) {
+    async function get_src_list(url, list) {
         const response = await fetch(url);
         const html = await response.text();
-        return html;
+        const re = /<a href=\"\/img\.html\?url=(.+?)\">/ig;
+        let mc;
+        while ((mc = re.exec(html)) !== null) {
+            list.push(mc[1]);
+        }
+    }
+    async function loop_page(url) {
+        const list = [];
+        get_src_list(url, list);
+        console.log(list);
+        //const response = await fetch(url);
+        //const html = await response.text();
+    }
+
+    async function demo() {
+        const html = await get_src_list("https://www.hitxhot.org/gallerys/OXBEb3FYSzRhODlSV0dzN2R1QTRzZz09.html");
+        //const win = window.open(null, "_blank");
+        //win.document.write(s);
     }
 
     const windowLoaded = async () => {
-        const s = await get_src_list("https://www.hitxhot.org/gallerys/OXBEb3FYSzRhODlSV0dzN2R1QTRzZz09.html");
-        const win = window.open(null, "_blank");
-        win.document.write(s);
+        const entry_title = document.querySelector("h1 .entry-title");
+        const btn_start = document.createElement("a");
+        btn_start.text = "聚合";
+        btn_start.href = "javascript:;";
+        btn_start.onclick = function () {
+            loop_page("https://www.hitxhot.org/gallerys/OXBEb3FYSzRhODlSV0dzN2R1QTRzZz09.html")
+        }
+        entry_title.append(btn_start);
         //console.log(s);
     };
     if (["interactive", "complete"].includes(document.readyState)) {
